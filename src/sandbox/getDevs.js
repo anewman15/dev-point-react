2a8288b502253a55dc0f4aae25605493da6a6f0f
Abdullah Numan <anewman15@hotmail.com>
2021-03-29 23:59:32 +0600

    Modify getDevs() func to receive session cookie

--- src/sandbox/getDevs.js
+++ src/sandbox/getDevs.js
@@ -1,5 +1,9 @@
 const getDevs = async ({ setDevelopers }) => {
-  const response = await fetch('http://localhost:3000/developers');
+  const response = await fetch('http://localhost:3000/developers', {
+    method: 'GET',
+    mode: 'cors',
+    credentials: 'include',
+  });
   const data = await response.json();
   setDevelopers(data.developers);
 };
