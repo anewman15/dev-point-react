f5ad2ba07b9a1e3be3888bdbc1acaa6ce7fcd59b
Abdullah Numan <anewman15@hotmail.com>
2021-03-30 20:55:33 +0600

    Create SignUpButton comp

---
+++ src/components/presentational/SignUpButton.js
@@ -0,0 +1,10 @@
+function SignUpButton() {
+  return (
+    <div>
+      <a href="/" className="button is-primary"><strong>Sign up</strong></a>
+      <a href="/" className="button is-light">Log in</a>
+    </div>
+  );
+}
+
+export default SignUpButton;
