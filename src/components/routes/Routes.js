87aa6d599c671ffe8083a9420a957a241b2e9a0c
Abdullah Numan <anewman15@hotmail.com>
2021-04-01 20:43:51 +0600

    Add routes for Home and LandingPage

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -1,13 +1,17 @@
 /* eslint-disable import/no-cycle */
 import { Switch, Route } from 'react-router-dom';
-import Main from '../containers/Main';
 import LogInForm from '../containers/LogInForm';
 import SignUpForm from '../containers/SignUpForm';
+import Home from '../containers/Home';
+import Main from '../containers/Main';
+import LandingPage from '../containers/LandingPage';
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={Main} />
+      <Route exact path="/land" component={LandingPage} />
+      <Route exact path="/home" component={Home} />
       <Route exact path="/login" component={LogInForm} />
       <Route exact path="/sign_up" component={SignUpForm} />
     </Switch>
