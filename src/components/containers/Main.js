7aa891baa11d100a2bab39c1eecd784bda04b480
Abdullah Numan <anewman15@hotmail.com>
2021-04-01 20:14:52 +0600

    Connect Main comp to redux store, manipulate state according to response

--- src/components/containers/Main.js
+++ src/components/containers/Main.js
@@ -1,21 +1,47 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-cycle */
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
 import validateSession from '../../sandbox/validateSession';
-import TopNav from './TopNav';
+import { saveAuthStatus } from '../../redux/actions/user';
+import LandingPage from './LandingPage';
+import LogInForm from './LogInForm';
 
-function Main() {
-  const [isLoggedIn, setIsLoggedIn] = useState(null);
+function Main({ authStatus, saveAuthStatus }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(authStatus);
 
   useEffect(() => {
-    validateSession({ setIsLoggedIn });
+    let cancelValidation = false;
+    if (!cancelValidation) {
+      validateSession()
+        .then(response => {
+          if (response.status === 200) {
+            setIsLoggedIn(true);
+            saveAuthStatus({ status: true });
+          } else {
+            setIsLoggedIn(false);
+            saveAuthStatus({ status: false });
+            console.log(response);
+          }
+        });
+    }
+    return () => {
+      cancelValidation = true;
+    };
   }, []);
 
-  console.log(isLoggedIn);
-
   return (
-    <div>
-      <TopNav isLoggedIn={isLoggedIn} />
-    </div>
+    isLoggedIn ? <LandingPage /> : <LogInForm />
   );
 }
 
-export default Main;
+Main.propTypes = {
+  authStatus: PropTypes.bool,
+}.isRequired;
+
+const mapStateToProps = state => ({
+  authStatus: state.authStatus,
+});
+
+export default connect(mapStateToProps, { saveAuthStatus })(Main);
