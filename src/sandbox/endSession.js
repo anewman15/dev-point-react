3d7e34d68016356ed433bbd04f228e3bbd868768
Abdullah Numan <anewman15@hotmail.com>
2021-04-01 20:11:42 +0600

    Modify endSession() func to return reponse or error

--- src/sandbox/endSession.js
+++ src/sandbox/endSession.js
@@ -1,13 +1,11 @@
 const endSession = async () => {
   try {
-    const reponse = await fetch('http://localhost:3000/users/sign_out', {
+    return await fetch('http://localhost:3000/users/sign_out', {
       method: 'DELETE',
       credentials: 'include',
     });
-    const data = await reponse.json();
-    console.log(data);
   } catch (e) {
-    console.log(e.message);
+    return e;
   }
 };
 
