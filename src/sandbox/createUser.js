b025823e19cdc61502a774455f5e95825c42d4f3
Abdullah Numan <anewman15@hotmail.com>
2021-04-02 21:29:43 +0600

    Modify createUser() func to return either response or error

--- src/sandbox/createUser.js
+++ src/sandbox/createUser.js
@@ -1,18 +1,16 @@
 const createUser = async userInfo => {
   try {
-    const response = await fetch('http://localhost:3000/users', {
+    return await fetch('http://localhost:3000/users', {
       method: 'POST',
       mode: 'cors',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(userInfo),
+      credentials: 'include',
     });
-    const data = await response.json();
-    return data;
   } catch (e) {
-    const data = e.message;
-    return data;
+    return e;
   }
 };
 
