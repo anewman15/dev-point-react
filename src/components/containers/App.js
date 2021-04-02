3903010383abf7184b4dd3a9c4c75e5f26de50a0
Abdullah Numan <anewman15@hotmail.com>
2021-04-02 18:23:53 +0600

    Provide store, TopNav and Routes to App.js

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,8 +1,18 @@
-import Login from './Login';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from '../../redux/store/store';
+import Routes from '../routes/Routes';
+import '../../styles/App.scss';
+import TopNav from './TopNav';
 
 function App() {
   return (
-    <Login />
+    <Provider store={store}>
+      <BrowserRouter>
+        <TopNav />
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
