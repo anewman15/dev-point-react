33c4ef751ad4884534f1745dc9a0c71a0b238266
Abdullah Numan <anewman15@hotmail.com>
2021-04-02 21:25:48 +0600

    Modify NavItemsSignedOut comp to use LogInButton and SignUpButton

--- src/components/presentational/NavItemsSignedOut.js
+++ src/components/presentational/NavItemsSignedOut.js
@@ -1,11 +1,12 @@
-import { Link } from 'react-router-dom';
+import SignUpButton from './SignUpButton';
+import LogInButton from './LogInButton';
 
 function NavItemsSignedOut() {
   return (
-    <div>
-      <Link to="/sign_up" className="button is-primary"><strong>Sign up</strong></Link>
-      <Link to="/login" className="button is-light">Log in</Link>
-    </div>
+    <>
+      <LogInButton />
+      <SignUpButton />
+    </>
   );
 }
 
