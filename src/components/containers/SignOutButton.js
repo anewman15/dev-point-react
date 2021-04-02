13dd03cf27dfb52bbf8ae697311341224dfc9555
Abdullah Numan <anewman15@hotmail.com>
2021-04-02 18:15:25 +0600

    Modify SignOutButton, replace Redirect with useHistory()

--- src/components/containers/SignOutButton.js
+++ src/components/containers/SignOutButton.js
@@ -2,12 +2,13 @@
 import { useState } from 'react';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { saveAuthStatus } from '../../redux/actions/user';
 import endSession from '../../sandbox/endSession';
 
 function SignOutButton({ authStatus, saveAuthStatus }) {
   const [isLoggedIn, setIsLoggedIn] = useState(authStatus);
+  const history = useHistory();
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -16,7 +17,7 @@ function SignOutButton({ authStatus, saveAuthStatus }) {
         if (response.status === 200) {
           setIsLoggedIn(false);
           saveAuthStatus({ status: false });
-            <Redirect to="/" />;
+          history.push('/');
         } else {
           console.log(response);
         }
