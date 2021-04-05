dce6110bc77c059fe47582b4b7583c872d9dd131
Abdullah Numan <anewman15@hotmail.com>
2021-04-05 15:58:27 +0600

    Connect NavItemsUsername to store, render username to navbar

--- src/components/containers/NavItemUsername.js
+++ src/components/containers/NavItemUsername.js
@@ -1,11 +1,25 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-expressions */
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-function NavItemUsername() {
+function NavItemUsername({ userDetails }) {
   return (
     <div className="navbar-item">
-      <Link className="has-text-dark" to="/home">Welcome, username</Link>
+      <Link className="has-text-dark" to="/">
+        {`Welcome, ${userDetails.username}`}
+      </Link>
     </div>
   );
 }
 
-export default NavItemUsername;
+NavItemUsername.propTypes = {
+  userDetails: PropTypes.object,
+}.isRequired;
+
+const mapStateToProps = state => ({
+  userDetails: state.userDetails,
+});
+
+export default connect(mapStateToProps)(NavItemUsername);
