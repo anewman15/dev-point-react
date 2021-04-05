acf4140f4dd8fe365bc7c66a2228be388862f2ac
Abdullah Numan <anewman15@hotmail.com>
2021-04-05 19:20:35 +0600

    Remove setDevelopers arg from getDevs func

--- src/sandbox/getDevs.js
+++ src/sandbox/getDevs.js
@@ -1,11 +1,13 @@
-const getDevs = async ({ setDevelopers }) => {
-  const response = await fetch('http://localhost:3000/developers', {
-    method: 'GET',
-    mode: 'cors',
-    credentials: 'include',
-  });
-  const data = await response.json();
-  setDevelopers(data.developers);
+const getDevs = async () => {
+  try {
+    return await fetch('http://localhost:3000/developers', {
+      method: 'GET',
+      mode: 'cors',
+      credentials: 'include',
+    });
+  } catch (e) {
+    return e;
+  }
 };
 
 export default getDevs;
