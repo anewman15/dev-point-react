8ae4a4d33a546e7b735e886e601ff9459d9c2423
Abdullah Numan <anewman15@hotmail.com>
2021-04-05 15:52:27 +0600

    Add userDetails reducer to combinedReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 import authStatus from './authStatus';
+import userDetails from './userDetails';
 
 const combinedReducer = combineReducers({
-  authStatus,
+  authStatus, userDetails,
 });
 
 export default combinedReducer;
