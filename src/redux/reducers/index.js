f1186a320ad6ac054564a496eb24e35104783549
Abdullah Numan <anewman15@hotmail.com>
2021-04-06 22:38:43 +0600

    Define currentDev reducer and add to combinedReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,16 @@
 import { combineReducers } from 'redux';
 import authStatus from './authStatus';
 import userDetails from './userDetails';
+import developers from './developers';
+import currentDev from './currentDev';
 
-const combinedReducer = combineReducers({
-  authStatus, userDetails,
-});
+const combinedReducer = combineReducers(
+  {
+    authStatus,
+    userDetails,
+    developers,
+    currentDev,
+  },
+);
 
 export default combinedReducer;
--- src/sandbox/getDev.js
+++
@@ -1,7 +0,0 @@
-const getDev = async ({ setDeveloper, id }) => {
-  const response = await fetch(`http://localhost:3000/developers/${id}`);
-  const data = await response.json();
-  setDeveloper(data.user);
-};
-
-export default getDev;
