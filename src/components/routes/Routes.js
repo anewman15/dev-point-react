85256aaa09e00e26416764e514106b06f77190b2
Abdullah Numan <anewman15@hotmail.com>
2021-04-08 14:28:49 +0600

    Add route for AppoinmentsPage

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -8,6 +8,7 @@ import LandingPage from '../containers/LandingPage';
 import DevelopersList from '../containers/DevelopersList';
 import DeveloperPage from '../containers/DeveloperPage';
 import BookAppointment from '../containers/BookAppointment';
+import AppoinmentsPage from '../containers/AppoinmentsPage';
 
 function Routes() {
   return (
@@ -20,6 +21,7 @@ function Routes() {
       <Route exact path="/developers" component={DevelopersList} />
       <Route exact path="/developers/:id" component={DeveloperPage} />
       <Route exact path="/book_appointment" component={BookAppointment} />
+      <Route exact path="/appointments" component={AppoinmentsPage} />
     </Switch>
   );
 }
