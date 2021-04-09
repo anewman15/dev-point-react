9ebe2d50bd3f1469f008d618ef53dde7aee44aa4
Abdullah Numan <anewman15@hotmail.com>
2021-04-09 14:04:04 +0600

    Rename handleSubmit to handleClick n SignOutButton, use history hook

--- src/components/containers/SignOutButton.js
+++ src/components/containers/SignOutButton.js
@@ -2,27 +2,29 @@
 import { useState } from 'react';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { saveAuthStatus } from '../../redux/actions/user';
 import endSession from '../../sandbox/endSession';
 
 function SignOutButton({ authStatus, saveAuthStatus }) {
   const [isLoggedIn, setIsLoggedIn] = useState(authStatus);
+  const history = useHistory();
 
-  const handleSubmit = e => {
+  const handleClick = e => {
     e.preventDefault();
     endSession()
       .then(response => {
         if (response.status === 200) {
           setIsLoggedIn(false);
           saveAuthStatus({ status: false });
+          history.push('/');
         }
       });
   };
 
   return (
     <div className="navbar-item">
-      <Link to="/" onClick={handleSubmit} className="button is-primary">
+      <Link to="/" onClick={handleClick} className="button is-primary">
         Sign Out
       </Link>
     </div>
