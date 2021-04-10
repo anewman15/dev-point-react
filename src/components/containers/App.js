e9b63d8312537b6a79006ba4b65196808b23350c
Abdullah Numan <anewman15@hotmail.com>
2021-04-10 17:18:46 +0600

    Replace TopNav in App.js with SideNav, add columns

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -3,14 +3,22 @@ import { BrowserRouter } from 'react-router-dom';
 import store from '../../redux/store/store';
 import Routes from '../routes/Routes';
 import '../../styles/App.scss';
-import TopNav from './TopNav';
+import SideNav from './SideNav';
+import BrandBar from './BrandBar';
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <TopNav />
-        <Routes />
+        <BrandBar />
+        <div className="columns is-centered">
+          <div className="column is-one-fifth">
+            <SideNav />
+          </div>
+          <div className="column is-four-fifths">
+            <Routes />
+          </div>
+        </div>
       </BrowserRouter>
     </Provider>
   );
