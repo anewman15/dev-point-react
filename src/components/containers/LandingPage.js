6520e7c9b827ae7d1226ed98bee1a538b2e6a3ff
Abdullah Numan <anewman15@hotmail.com>
2021-04-10 14:42:47 +0600

    Add content and styling LandingPage

--- src/components/containers/LandingPage.js
+++ src/components/containers/LandingPage.js
@@ -1,7 +1,31 @@
+import { Link } from 'react-router-dom';
+
 function LandingPage() {
   return (
-    <div>
-      Hi, Welcome
+    <div className="bg-code">
+      <div className="columns is-centered">
+        <div className="my-7 column is-four-fifths">
+          <div className="p-6 bd-filter border-warning has-text-white">
+            <p className="is-size-2 has-text-centered">
+              Welcome to DevPoint
+            </p>
+            <p className="my-3 p-2 is-size-4 has-text-centered">
+              Our experienced developers specialize in latest frameworks and technologies, and have
+              a track record of successful project deliveries across multiple
+              domains in the software industry
+            </p>
+            <p className="my-3 p-2 is-size-4 has-text-centered">
+              Our platform allows you to meet our developers individually for a
+              free online consultation and build rapport for future collaboration
+            </p>
+          </div>
+          <div className="my-6 mx-5 has-text-centered is-flex is-justify-content-center is-align-items-center">
+            <Link to="/sign_up" className="button is-success is-large is-uppercase">Sign Up Now</Link>
+            <p className="px-4 has-text-white has-text-weight-bold">or</p>
+            <Link to="/login" className="button is-light is-small has-text-dark has-text-weight-bold">Log into your account</Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
