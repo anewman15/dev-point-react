ee9aef2f1fe4680b9f35ae4fa403a2476b5e43bd
Abdullah Numan <anewman15@hotmail.com>
2021-04-15 19:01:35 +0600

    Add content to Home.js

--- src/components/containers/Home.js
+++ src/components/containers/Home.js
@@ -8,7 +8,7 @@ const Home = () => (
 
     <div className="my-5 is-flex is-justify-content-center">
       <a
-        className="button is-warning is-large has-text-success has-text-center"
+        className="button is-warning has-text-success has-text-center"
         href="https://www.furniturebari.com/"
         target="_blank"
         rel="noreferrer"
@@ -17,12 +17,18 @@ const Home = () => (
         <span className="px-2 has-text-weight-bold">NOW</span>
       </a>
     </div>
-    <div className="my-6 px-3 is-flex is-justify-content-center is-align-items-center">
-      <p className="has-text-center">
-        Or better yet, navigate to our
-        <Link className="px-2 has-text-primary has-text-weight-bold" to="/developers">Developers </Link>
-        page and get in touch with them
+    <div className="my-6 px-3">
+      <p className="has-text-center py-3">
+        Pun aside, this page will soon allow you to add a
+        profile picture, company info and an about me/us section
       </p>
+      <div className="py-3 is-flex is-justify-content-center is-align-items-center">
+        <p className="has-text-center">
+          For now though, navigate to our
+          <Link className="px-2 has-text-primary has-text-weight-bold" to="/developers">Developers </Link>
+          page and get in touch with them
+        </p>
+      </div>
     </div>
   </div>
 );
