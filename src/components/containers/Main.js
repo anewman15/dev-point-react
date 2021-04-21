0804d02bd243fde8fdf6622b571eb03b163a6352
Abdullah Numan <anewman15@hotmail.com>
2021-04-21 19:31:50 +0600

    Delete validateSession() func remove call to validate session in Main.js

--- src/components/containers/Main.js
+++ src/components/containers/Main.js
@@ -1,33 +1,9 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable import/no-cycle */
-import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
-import validateSession from '../../sandbox/validateSession';
-import { saveAuthStatus } from '../../redux/actions/user';
 import Home from './Home';
 import LandingPage from './LandingPage';
 
-function Main({ authStatus, saveAuthStatus }) {
-  const [isLoggedIn, setIsLoggedIn] = useState(authStatus);
-
-  useEffect(() => {
-    let cancelValidation = false;
-    if (!cancelValidation) {
-      validateSession()
-        .then(response => response.json())
-        .then(data => {
-          if (data.user) {
-            setIsLoggedIn(true);
-            saveAuthStatus({ status: true });
-          }
-        });
-    }
-    return () => {
-      cancelValidation = true;
-    };
-  }, []);
-
+function Main({ authStatus }) {
   const content = authStatus ? <Home /> : <LandingPage />;
 
   return content;
@@ -41,4 +17,4 @@ const mapStateToProps = state => ({
   authStatus: state.authStatus,
 });
 
-export default connect(mapStateToProps, { saveAuthStatus })(Main);
+export default connect(mapStateToProps)(Main);
