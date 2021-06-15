f01ef1b30f407760c6b490e9346f59b69a2e4705
Abdullah Numan <anewman15@hotmail.com>
2021-06-15 21:28:09 +0600

    Create SomethingWrong component

---
+++ src/components/presentational/SomethingWrong.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SomethingWrong = () => (
+  <div>
+    <h1>Something Went Wrong</h1>
+    <p>Please try refreshing and make sure you&apos;re connected to the Internet</p>
+  </div>
+);
+
+export default SomethingWrong;
