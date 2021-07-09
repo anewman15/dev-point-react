eb2ec8ff8ffeda0b97e8d979eeabfd0d36e958f8
Abdullah Numan <anewman15@hotmail.com>
2021-07-09 23:37:01 +0600

    Add EditProfile to Routes

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -10,6 +10,7 @@ import DeveloperPage from '../containers/DeveloperPage';
 import BookAppointment from '../containers/BookAppointment';
 import AppoinmentsPage from '../containers/AppoinmentsPage';
 import NotFound from '../presentational/NotFound';
+import EditProfile from '../containers/EditProfile';
 
 function Routes() {
   return (
@@ -17,6 +18,7 @@ function Routes() {
       <Route exact path="/" component={Main} />
       <Route exact path="/land" component={LandingPage} />
       <Route exact path="/home" component={Home} />
+      <Route exact path="/edit_profile" component={EditProfile} />
       <Route exact path="/login" component={LogInForm} />
       <Route exact path="/sign_up" component={SignUpForm} />
       <Route exact path="/developers" component={DevelopersList} />
