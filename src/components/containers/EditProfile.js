15c38386d92ab2ee42680224a45584891bda68c4
Abdullah Numan <anewman15@hotmail.com>
2021-07-11 23:15:39 +0600

    Set input fields to required in EditProfile comp

--- src/components/containers/EditProfile.js
+++ src/components/containers/EditProfile.js
@@ -10,7 +10,7 @@ const EditProfile = () => (
       <form className="py-2 is-flex is-justify-content-center is-align-items-center">
         <div className="file mx-2">
           <label className="file-label">
-            <input className="file-input" type="file" name="profile_image" />
+            <input className="file-input" type="file" name="profile_image" required />
             <span className="file-cta">
               <span className="file-icon">
                 <i className="fas fa-upload" />
@@ -33,8 +33,9 @@ const EditProfile = () => (
         <form>
           <div className="is-flex is-justify-content-start is-align-items-center">
             <div className="control is-flex is-justify-content-center is-align-items-center">
-              <input className="input mx-2" type="text" placeholder="FirstName" />
-              <input className="input mx-2" type="text" placeholder="LastName" />
+              <input className="input mx-2" type="text" placeholder="FirstName" required />
+              <input className="input mx-2" type="text" placeholder="Other Names" required />
+              <input className="input mx-2" type="text" placeholder="LastName" required />
             </div>
             <div className="control">
               <button type="submit" className="button is-primary">Save</button>
@@ -48,17 +49,17 @@ const EditProfile = () => (
           <div className="is-flex is-justify-content-start is-align-items-flex-end">
             <div className="">
               <div className="control is-flex is-justify-content-center is-align-items-center my-2">
-                <input className="input mx-2" type="text" placeholder="Job Title" />
-                <input className="input mx-2" type="text" placeholder="Employer" />
+                <input className="input mx-2" type="text" placeholder="Job Title" required />
+                <input className="input mx-2" type="text" placeholder="Employer" required />
               </div>
               <div className="control is-flex is-justify-content-center is-align-items-center my-2">
                 <label htmlFor="start-date" className="mx-2">
                   Start Date
-                  <input className="input" type="month" name="start-date" />
+                  <input className="input" type="month" name="start-date" required />
                 </label>
                 <label htmlFor="end-date" className="mx-2">
                   End Date
-                  <input className="input" type="month" name="end-date" />
+                  <input className="input" type="month" name="end-date" required />
                 </label>
               </div>
             </div>
@@ -75,11 +76,11 @@ const EditProfile = () => (
             <div className="control is-flex is-justify-content-center is-align-items-center my-2">
               <label htmlFor="link-name" className="mx-2">
                 Website Name
-                <input className="input" type="text" name="link-name" placeholder="GitHub" />
+                <input className="input" type="text" name="link-name" placeholder="GitHub" required />
               </label>
               <label htmlFor="link-url" className="mx-2">
                 Url
-                <input className="input" type="url" name="link-url" placeholder="user@github.com" />
+                <input className="input" type="url" name="link-url" placeholder="user@github.com" required />
               </label>
             </div>
             <div className="control my-2">
@@ -94,7 +95,7 @@ const EditProfile = () => (
           <div className="is-flex is-justify-content-start is-align-items-flex-end my-3">
             <label htmlFor="link-url" className="mx-2">
               Video Call Link
-              <input className="input" type="url" name="link-url" placeholder="https://zoom.us/j/randomzoomidwithsomerandomcharsandnums" />
+              <input className="input" type="url" name="link-url" placeholder="https://zoom.us/j/randomzoomidwithsomerandomcharsandnums" required />
             </label>
             <div className="control">
               <button type="submit" className="button is-primary">Add</button>
@@ -103,9 +104,9 @@ const EditProfile = () => (
         </form>
         <form>
           <div className="is-flex is-justify-content-start is-align-items-flex-end my-3">
-            <label htmlFor="link-url" className="mx-2">
+            <label htmlFor="phone-number" className="mx-2">
               Phone Number
-              <input className="input" type="url" name="link-url" placeholder="https://zoom.us/j/randomzoomidwithsomerandomcharsandnums" />
+              <input className="input" type="url" name="phone-number" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="123-345-5678" required />
             </label>
             <div className="control">
               <button type="submit" className="button is-primary">Add</button>
