8381b5003730ac53f97c50426cbd1b29ba42e1ea
Abdullah Numan <anewman15@hotmail.com>
2021-07-11 22:44:05 +0600

    Add Social Links section in EditProfile comp

--- src/components/containers/EditProfile.js
+++ src/components/containers/EditProfile.js
@@ -45,19 +45,45 @@ const EditProfile = () => (
       <div className="my-6">
         <h1 className="my-1 px-2 is-size-5 has-text-weight-bold">Employment Info</h1>
         <form>
-          <div className="is-flex is-justify-content-start is-align-items-center">
+          <div className="is-flex is-justify-content-start is-align-items-flex-end">
             <div className="">
               <div className="control is-flex is-justify-content-center is-align-items-center my-2">
                 <input className="input mx-2" type="text" placeholder="Job Title" />
                 <input className="input mx-2" type="text" placeholder="Employer" />
               </div>
               <div className="control is-flex is-justify-content-center is-align-items-center my-2">
-                <input className="input mx-2" type="date" placeholder="Start Date" />
-                <input className="input mx-2" type="date" placeholder="End Date" />
+                <label htmlFor="start-date" className="mx-2">
+                  Start Date
+                  <input className="input" type="month" name="start-date" />
+                </label>
+                <label htmlFor="end-date" className="mx-2">
+                  End Date
+                  <input className="input" type="month" name="end-date" />
+                </label>
               </div>
             </div>
-            <div className="control">
+            <div className="control my-2">
               <button type="submit" className="button is-primary">Save</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div className="my-6">
+        <h1 className="my-1 px-2 is-size-5 has-text-weight-bold">Social Links</h1>
+        <form>
+          <div className="is-flex is-justify-content-start is-align-items-flex-end">
+            <div className="control is-flex is-justify-content-center is-align-items-center my-2">
+              <label htmlFor="link-name" className="mx-2">
+                Website Name
+                <input className="input" type="text" name="link-name" placeholder="GitHub" />
+              </label>
+              <label htmlFor="link-url" className="mx-2">
+                Url
+                <input className="input" type="url" name="link-url" placeholder="user@github.com" />
+              </label>
+            </div>
+            <div className="control my-2">
+              <button type="submit" className="button is-primary">Add Link</button>
             </div>
           </div>
         </form>
