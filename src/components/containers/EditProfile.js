be299826dd5dbd5a6354f8e0e6de2f259d2a5d22
Abdullah Numan <anewman15@hotmail.com>
2021-07-13 00:14:45 +0600

    Refactor Employment Info section into EditProfileEmploymentSection comp

--- src/components/containers/EditProfile.js
+++ src/components/containers/EditProfile.js
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 
 import EditProfileImageSection from './EditProfileImageSection';
+import EditProfileEmploymentSection from './EditProfileEmploymentSection';
 import EditProfileNameSection from './EditProfileNameSection';
 
 const EditProfile = () => (
@@ -8,32 +9,7 @@ const EditProfile = () => (
     <h1 className="mb-4 px-2 is-size-1 has-text-centered has-text-weight-bold">Edit Profile</h1>
     <EditProfileImageSection />
     <EditProfileNameSection />
-    <div className="my-6">
-      <h1 className="my-1 px-2 is-size-5 has-text-weight-bold">Employment Info</h1>
-      <form>
-        <div className="is-flex is-justify-content-start is-align-items-flex-end">
-          <div className="">
-            <div className="control is-flex is-justify-content-center is-align-items-center my-2">
-              <input className="input mx-2" type="text" placeholder="Job Title" required />
-              <input className="input mx-2" type="text" placeholder="Employer" required />
-            </div>
-            <div className="control is-flex is-justify-content-center is-align-items-center my-2">
-              <label htmlFor="start-date" className="mx-2">
-                Start Date
-                <input className="input" type="month" name="start-date" required />
-              </label>
-              <label htmlFor="end-date" className="mx-2">
-                End Date
-                <input className="input" type="month" name="end-date" required />
-              </label>
-            </div>
-          </div>
-          <div className="control my-2">
-            <button type="submit" className="button is-primary">Save</button>
-          </div>
-        </div>
-      </form>
-    </div>
+    <EditProfileEmploymentSection />
     <div className="my-6">
       <h1 className="my-1 px-2 is-size-5 has-text-weight-bold">Social Links</h1>
       <form>
