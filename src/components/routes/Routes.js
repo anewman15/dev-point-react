071ad6a824bf0358ef9e1aa3fd17a5f7d5476900
Abdullah Numan <anewman15@hotmail.com>
2021-07-16 19:54:34 +0600

    Connect Routes comp to redux store and use AuthenticatedRoute for appointments

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -1,5 +1,7 @@
 /* eslint-disable import/no-cycle */
 import { Switch, Route } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
 import LogInForm from '../containers/LogInForm';
 import SignUpForm from '../containers/SignUpForm';
 import Home from '../containers/Home';
@@ -11,23 +13,30 @@ import BookAppointment from '../containers/BookAppointment';
 import AppoinmentsPage from '../containers/AppoinmentsPage';
 import NotFound from '../presentational/NotFound';
 import EditProfile from '../containers/EditProfile';
+import AuthenticatedRoute from './AuthenticatedRoute';
 
-function Routes() {
-  return (
-    <Switch>
-      <Route exact path="/" component={Main} />
-      <Route exact path="/land" component={LandingPage} />
-      <Route exact path="/home" component={Home} />
-      <Route exact path="/edit_profile" component={EditProfile} />
-      <Route exact path="/login" component={LogInForm} />
-      <Route exact path="/sign_up" component={SignUpForm} />
-      <Route exact path="/developers" component={DevelopersList} />
-      <Route exact path="/developers/:id" component={DeveloperPage} />
-      <Route exact path="/book_appointment" component={BookAppointment} />
-      <Route exact path="/appointments" component={AppoinmentsPage} />
-      <Route path="*" component={NotFound} />
-    </Switch>
-  );
-}
+const Routes = ({ currentUser }) => (
+  <Switch>
+    <Route exact path="/" component={Main} />
+    <Route exact path="/land" component={LandingPage} />
+    <Route exact path="/home" component={Home} />
+    <Route exact path="/edit_profile" component={EditProfile} />
+    <Route exact path="/login" component={LogInForm} />
+    <Route exact path="/sign_up" component={SignUpForm} />
+    <Route exact path="/developers" component={DevelopersList} />
+    <Route exact path="/developers/:id" component={DeveloperPage} />
+    <Route exact path="/book_appointment" component={BookAppointment} />
+    <AuthenticatedRoute exact path="/appointments" currentUser={currentUser} component={AppoinmentsPage} />
+    <Route path="*" component={NotFound} />
+  </Switch>
+);
 
-export default Routes;
+Routes.propTypes = {
+  currentUser: PropTypes.object,
+}.isRequired;
+
+const mapStateToProps = state => ({
+  currentUser: state.currentUser,
+});
+
+export default connect(mapStateToProps)(Routes);
