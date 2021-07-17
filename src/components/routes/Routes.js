1daffca80b84cbcb68797fdf6b276318c83042db
Abdullah Numan <anewman15@hotmail.com>
2021-07-17 21:30:44 +0600

    Make LogInForm route a AuthenticationRoute and remove currentUser.id check

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -13,13 +13,14 @@ import AppoinmentsPage from '../containers/AppoinmentsPage';
 import NotFound from '../presentational/NotFound';
 import EditProfile from '../containers/EditProfile';
 import AuthenticatedRoute from './AuthenticatedRoute';
+import AuthenticationRoute from './AuthenticationRoute';
 
 const Routes = ({ currentUser }) => (
   <Switch>
     <Route exact path="/" component={Main} />
     <AuthenticatedRoute exact path="/home" currentUser={currentUser} component={Home} />
     <AuthenticatedRoute exact path="/edit_profile" currentUser={currentUser} component={EditProfile} />
-    <Route exact path="/login" component={LogInForm} />
+    <AuthenticationRoute exact path="/login" currentUser={currentUser} component={LogInForm} />
     <Route exact path="/sign_up" component={SignUpForm} />
     <AuthenticatedRoute exact path="/developers" currentUser={currentUser} component={DevelopersList} />
     <AuthenticatedRoute exact path="/developers/:id" currentUser={currentUser} component={DeveloperPage} />
