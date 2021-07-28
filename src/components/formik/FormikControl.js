782b8f1ad77b93c7fc23599db2d42ff4dac881ca
Abdullah Numan <anewman15@hotmail.com>
2021-07-29 00:03:58 +0600

    Pass ...rest props to FormikControl comp

--- src/components/formik/FormikControl.js
+++ src/components/formik/FormikControl.js
@@ -1,16 +1,19 @@
-import { PropType } from 'prop-types';
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import { PropTypes } from 'prop-types';
+import ImageInput from './ImageInput';
 
 const FormikControl = props => {
-  const { control } = props;
+  const { control, ...rest } = props;
   switch (control) {
-    case 'image': return <ImageInput />;
+    case 'image':
+      return <ImageInput {...rest} />;
     default: return null;
   }
 };
 
 FormikControl.propTypes = {
-  control: PropType.string,
+  control: PropTypes.string,
 }.isRequired;
 
 export default FormikControl;
