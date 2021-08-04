3bef60cb581779efc98f0d9c51a5af569b154982
Abdullah Numan <anewman15@hotmail.com>
2021-08-04 15:19:28 +0600

    Modify uploadProfileImage function

--- src/sandbox/uploadProfileImage.js
+++ src/sandbox/uploadProfileImage.js
@@ -1,24 +1,34 @@
+/* eslint-disable consistent-return */
 import fileHash from '../utils/fileHash';
 import getPresignedUrl from './getPresignedUrl';
 import storeToS3Bucket from './storeToS3Bucket';
 import updateProfileImage from './updateProfileImage';
 
 const uploadProfileImage = async userInfo => {
-  const { fileBinary } = userInfo;
-  try {
-    const fileMD5 = await fileHash(fileBinary);
-    const presignedUrlResponse = await getPresignedUrl(fileBinary, fileMD5);
+  // const { fileBinary } = userInfo;
+  // try {
+  //   const fileMD5 = await fileHash(fileBinary);
 
-    const presignedUrlParams = await presignedUrlResponse.json();
+  //   let presignedUrlParams;
+  //   const presignedUrlResponse = getPresignedUrl(fileBinary, fileMD5);
 
-    const s3Response = await storeToS3Bucket(presignedUrlParams, fileBinary);
+  //   presignedUrlResponse.then(response => {
+  //     if (response.status === 200) {
+  //       return response.json();
+  //     }
+  //   })
+  //     .then(data => {
+  //       presignedUrlParams = data;
+  //     });
 
-    if (s3Response.status !== 200) throw (new Error('Error while uploading to storage service.'));
+  //   const s3Response = await storeToS3Bucket(presignedUrlParams, fileBinary);
 
-    return await updateProfileImage(userInfo, presignedUrlParams.blob_signed_id);
-  } catch (e) {
-    return e;
-  }
+  //   if (s3Response.status !== 200) throw (new Error('Error while uploading to storage service.'));
+
+  //   return await updateProfileImage(userInfo, presignedUrlParams.blob_signed_id);
+  // } catch (e) {
+  //   return e;
+  // }
 };
 
 export default uploadProfileImage;
