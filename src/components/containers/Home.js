c3c27c4a0659065bc8ceafbd1fbccfb5b7427e31
Abdullah Numan <anewman15@hotmail.com>
2021-08-11 21:30:19 +0600

    Modify text of Home page

--- src/components/containers/Home.js
+++ src/components/containers/Home.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const Home = ({ currentUser }) => {
-  const { first_name, profile_image_url } = currentUser;
+  const { username, first_name, profile_image_url } = currentUser;
   return (
     <div className="my-3 p-3">
       <div className="my-6 py-4 is-flex is-justify-content-space-around is-align-items-center is-align-content-center">
@@ -13,20 +13,19 @@ const Home = ({ currentUser }) => {
         </div>
         <p className="px-2 is-size-1 has-text-centered has-text-weight-bold mx-3">
           Welcome
-          {first_name && ` ${first_name}!`}
+          {first_name ? ` ${first_name} !` : ` ${username} !`}
         </p>
       </div>
       <div className="my-3 px-3">
         <p className="has-text-centered py-3">
-          This page will soon allow you to add your name, a
-          profile picture, company info and skills
+          To add a profile image and other details, please visit the Edit Profile page
         </p>
         <div className="is-flex is-justify-content-center is-align-items-center">
           <Link to="/edit_profile" className="button is-success my-5">Edit Profile</Link>
         </div>
         <div className="py-3 is-flex is-justify-content-center is-align-items-center">
           <p className="has-text-centered">
-            For now though, navigate to our
+            To get into business, navigate to our
             <Link className="px-2 has-text-primary has-text-weight-bold" to="/developers">Developers </Link>
             page and get in touch with them
           </p>
