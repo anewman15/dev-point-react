ce941f3e80146838ca46463b7021c3d109c1234f
Abdullah Numan <anewman15@hotmail.com>
2021-08-11 21:43:59 +0600

    Disable components in EditProfile page, update content

--- src/components/containers/EditProfile.js
+++ src/components/containers/EditProfile.js
@@ -1,19 +1,27 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 
 import EditProfileImageSection from './EditProfileImageSection';
-import EditProfileEmploymentSection from './EditProfileEmploymentSection';
-import EditProfileNameSection from './EditProfileNameSection';
-import EditProfileLinksSection from './EditProfileLinksSection';
-import EditProfileContactSection from './EditProfileContactSection';
+// import EditProfileEmploymentSection from './EditProfileEmploymentSection';
+// import EditProfileNameSection from './EditProfileNameSection';
+// import EditProfileLinksSection from './EditProfileLinksSection';
+// import EditProfileContactSection from './EditProfileContactSection';
 
 const EditProfile = () => (
   <div className="my-1 p-3">
     <h1 className="mb-4 px-2 is-size-1 has-text-centered has-text-weight-bold">Edit Profile</h1>
+    <div className="mb-4 p-5 notification is-danger is-light">
+      <p className="my-2">
+        This page will soon allow you to add your name, employment and contact information.
+      </p>
+      <p>
+        For now though, go ahead and upload your profile image.
+      </p>
+    </div>
     <EditProfileImageSection />
-    <EditProfileNameSection />
+    {/* <EditProfileNameSection />
     <EditProfileEmploymentSection />
     <EditProfileLinksSection />
-    <EditProfileContactSection />
+    <EditProfileContactSection /> */}
   </div>
 );
 
