62b9b08cc13bb8a3cbd3008198d5e2d001ce114e
Abdullah Numan <anewman15@hotmail.com>
2021-10-04 23:45:35 +0600

    Remove divs from App comp, change class names

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -3,20 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 import store from '../../redux/store/store';
 import Routes from '../routes/Routes';
 import SideNav from './SideNav';
-import BrandBar from './BrandBar';
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <BrandBar />
-        <div className="columns is-desktop is-centered">
-          <div className="column is-one-fifth-desktop">
-            <SideNav />
-          </div>
-          <div className="column is-four-fifths-desktop">
-            <Routes />
-          </div>
+        <SideNav />
+        <div className="container mx-auto">
+          <Routes />
         </div>
       </BrowserRouter>
     </Provider>
