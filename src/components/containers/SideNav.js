0fc0be0aa9a95ca2b0235d6e6f6a7030b0635ac3
Abdullah Numan <anewman15@hotmail.com>
2021-10-04 23:45:04 +0600

    Change class names for SideNav comp

--- src/components/containers/SideNav.js
+++ src/components/containers/SideNav.js
@@ -1,15 +1,23 @@
 /* eslint-disable react/self-closing-comp */
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import NavItemsSignedOut from '../presentational/NavItemsSignedOut';
 import NavItemsLoggedIn from './NavItemsLoggedIn';
 
 function SideNav({ currentUser }) {
   return (
-    <nav id="side-nav" className="navbar-menu my-5">
-      <div className="menu-list">
-        {currentUser.id ? <NavItemsLoggedIn /> : <NavItemsSignedOut /> }
+    <nav className="fixed mx-auto w-full">
+      <div className="px-2 flex justify-between items-center bg-yellow-300">
+        <div className="mx-2">
+          <Link className="" to="/">
+            <img src={`${process.env.PUBLIC_URL}/logo512.png`} alt="dev-lookup-logo" width="60" height="60" />
+          </Link>
+        </div>
+        <div className="flex space-x-2 items-center">
+          {currentUser.id ? <NavItemsLoggedIn /> : <NavItemsSignedOut /> }
+        </div>
       </div>
     </nav>
   );
