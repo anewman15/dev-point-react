dd12c7a1547a9053b0ff9076651415e0a477e23d
Abdullah Numan <anewman15@hotmail.com>
2021-10-05 23:48:55 +0600

    Add px to navbar for md screen

--- src/components/containers/SideNav.js
+++ src/components/containers/SideNav.js
@@ -9,7 +9,7 @@ import NavItemsLoggedIn from './NavItemsLoggedIn';
 function SideNav({ currentUser }) {
   return (
     <nav className="fixed mx-auto w-full z-1000 top-0">
-      <div className="px-2 flex justify-between items-center bg-yellow-300">
+      <div className="px-2 md:px-12 flex justify-between items-center bg-yellow-300">
         <div className="mx-2">
           <Link className="" to="/">
             <img src={`${process.env.PUBLIC_URL}/logo512.png`} alt="dev-lookup-logo" width="60" height="60" />
