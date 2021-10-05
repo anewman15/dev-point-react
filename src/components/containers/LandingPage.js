889b93a9aede746f31ea5a03976cb497e0984cd5
Abdullah Numan <anewman15@hotmail.com>
2021-10-06 00:26:01 +0600

    Add tailwind classes to LandingPage comp

--- src/components/containers/LandingPage.js
+++ src/components/containers/LandingPage.js
@@ -3,31 +3,33 @@ import { Link } from 'react-router-dom';
 function LandingPage() {
   return (
     <div className="bg-code">
-      <div className="columns is-centered">
-        <div className="my-6 column is-four-fifths">
-          <div className="p-6 bd-filter border-warning has-text-white">
-            <p className="is-size-2 has-text-centered">
+      <div className="">
+        <div className="">
+          <div className="p-12 bd-filter text-white">
+            <p className="p-8 text-6xl text-center font-medium">
               Welcome to DevPoint
             </p>
-            <p className="my-3 p-2 is-size-4 has-text-centered">
-              Our experienced developers specialize in latest frameworks and technologies, and have
-              a track record of successful project deliveries across multiple
-              domains in the software industry
-            </p>
-            <p className="my-3 p-2 is-size-4 has-text-centered">
-              Our platform allows you to meet our developers individually for a
-              free online consultation and build rapport for future collaboration
-            </p>
+            <div className="my-8 text-2xl text-center">
+              <p className="my-3 p-2">
+                Our experienced developers specialize in latest frameworks and technologies,
+                and have a track record of successful project deliveries across multiple
+                domains in the software industry
+              </p>
+              <p className="my-3 p-2">
+                Our platform allows you to meet our developers individually for a
+                free online consultation and build rapport for future collaboration
+              </p>
+            </div>
           </div>
-          <div className="columns is-centered my-6 mx-5">
-            <div className="column has-text-centered is-flex is-justify-content-center is-align-items-center">
-              <Link to="/sign_up" className="button is-success is-large is-uppercase">Sign Up Now</Link>
+          <div className="my-20 flex justify-center space-x-4 items-center my-6 mx-5">
+            <div className="text-center flex is-justify-center items-center">
+              <Link to="/sign_up" className="btn btn-success btn-lg">Sign Up Now</Link>
             </div>
-            <div className="column has-text-centered is-flex is-justify-content-center is-align-items-center">
-              <p className="px-4 has-text-white has-text-weight-bold">or</p>
+            <div className="text-center flex is-justify-center items-center">
+              <p className="px-4 text-white font-bold">or</p>
             </div>
-            <div className="column has-text-centered is-flex is-justify-content-center is-align-items-center">
-              <Link to="/login" className="button is-light is-small has-text-dark has-text-weight-bold">Log into your account</Link>
+            <div className="text-center flex is-justify-center items-center">
+              <Link to="/login" className="btn btn-secondary font-bold">Log into your account</Link>
             </div>
           </div>
         </div>
