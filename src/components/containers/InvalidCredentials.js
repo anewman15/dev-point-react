185a67623a02ee3f058b369b6608c67be414ea5f
Abdullah Numan <anewman15@hotmail.com>
2021-10-07 21:19:32 +0600

    Style error components

--- src/components/containers/InvalidCredentials.js
+++ src/components/containers/InvalidCredentials.js
@@ -1,5 +1,5 @@
 const InvalidCredentials = () => (
-  <div className="notification is-danger is-light">
+  <div className="p-4 mx-auto w-80 rounded border border-red-400 bg-red-100 text-red-600 text-center">
     Invalid credentials. Please make sure the email/password is right
   </div>
 );
