4a54e910a2c58d92d53bfe6d20a3ba11c5b989d3
Abdullah Numan <anewman15@hotmail.com>
2021-10-08 21:15:52 +0600

    Add margin y to HostedAppointments com[

--- src/components/containers/HostedAppointments.js
+++ src/components/containers/HostedAppointments.js
@@ -5,7 +5,7 @@ import { formattedDate, timeLeft } from '../../utils/date';
 
 function HostedAppointments({ hostedAppointments }) {
   const appointmentsList = hostedAppointments && hostedAppointments.map(appointment => (
-    <div key={`${appointment.id}`} className="container mx-auto w-11/12 md:w-9/12 lg:w-8/12 bg-warning-300 px-8 py-6 rounded filter drop-shadow-xl">
+    <div key={`${appointment.id}`} className="container mx-auto my-10 w-11/12 md:w-9/12 lg:w-8/12 bg-warning-300 px-8 py-6 rounded filter drop-shadow-xl">
       <div className="flex flex-wrap justify-between items-center space-x-4">
         <p className="text-sm text-primary-500 font-bold">
           {appointment.location}
