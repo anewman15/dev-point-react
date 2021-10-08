e68ad2f707944592695ed7f21b00cca09d4253b8
Abdullah Numan <anewman15@hotmail.com>
2021-10-09 00:55:18 +0600

    Add height, bg to Home com[

--- src/components/containers/Home.js
+++ src/components/containers/Home.js
@@ -6,8 +6,8 @@ import { Link } from 'react-router-dom';
 const Home = ({ currentUser }) => {
   const { username, first_name, profile_image_url } = currentUser;
   return (
-    <div className="my-3 mx-auto px-2 md:px-8">
-      <div className="mx-auto my-6 p-4 rounded border border-yellow-500">
+    <div className="my-3 mx-auto px-2 md:px-8 h-screen filter drop-shadow-2xl">
+      <div className="mx-auto md:w-11/12 lg:w-10/12 my-6 p-2 rounded border border-yellow-50 h-full bg-gray-50">
         <div className="p-4 flex justify-center space-x-8 items-center content-center">
           <div className="w-32 rounded-full">
             <img className="rounded-full" src={profile_image_url || `${process.env.PUBLIC_URL}/dev.png`} alt="profile_image" />
@@ -18,22 +18,18 @@ const Home = ({ currentUser }) => {
           </p>
         </div>
 
-        <hr className="border-1 border-gray-200" />
+        <hr className="mx-8 border-1 border-gray-200" />
 
-        <div className="my-8 mx-auto w-10/12 text-gray-800">
-          <div className="flex justify-center">
-            <p className="py-3 leading-loose">
-              To add a profile image and other details, please visit the Edit Profile page
-              <Link to="/edit_profile" className="btn btn-warning ml-4">Edit&nbsp;Profile</Link>
-            </p>
-          </div>
-          <div className="flex justify-center">
-            <p className="py-3">
-              To get into business, navigate to our
-              <Link className="px-2 link-primary" to="/developers">Developers </Link>
-              page and get in touch with them
-            </p>
-          </div>
+        <div className="my-8 px-4 mx-auto w-8/12 text-gray-800">
+          <p className="py-3 leading-loose">
+            To add a profile image and other details, please visit the Edit Profile page
+            <Link to="/edit_profile" className="btn btn-warning ml-4">Edit&nbsp;Profile</Link>
+          </p>
+          <p className="py-3">
+            To get into business, navigate to our
+            <Link className="px-2 link-primary" to="/developers">Developers </Link>
+            page and get in touch with them
+          </p>
         </div>
       </div>
     </div>
