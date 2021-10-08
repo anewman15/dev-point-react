e8fc2c6ab5e191131f3b718a4a1830f404795a2c
Abdullah Numan <anewman15@hotmail.com>
2021-10-08 18:51:40 +0600

    Change logo width

--- src/components/containers/SideNav.js
+++ src/components/containers/SideNav.js
@@ -10,9 +10,9 @@ function SideNav({ currentUser }) {
   return (
     <nav className="sticky mx-auto w-full z-10 top-0">
       <div className="px-2 md:px-12 flex justify-between items-center bg-yellow-400">
-        <div className="mr-2 py-1">
+        <div className="mr-2">
           <Link className="" to="/">
-            <img src={`${process.env.PUBLIC_URL}/logo512.png`} alt="dev-lookup-logo" width="120" height="120" />
+            <img src={`${process.env.PUBLIC_URL}/logo512.png`} alt="dev-lookup-logo" width="120" />
           </Link>
         </div>
         <div className="flex space-x-6 items-center text-l">
