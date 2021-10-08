e21ac833df5c2eacce8a063d92e780b53b64613e
Abdullah Numan <anewman15@hotmail.com>
2021-10-08 18:53:06 +0600

    Style Home comp with tailwind classes

--- src/components/containers/Home.js
+++ src/components/containers/Home.js
@@ -6,29 +6,34 @@ import { Link } from 'react-router-dom';
 const Home = ({ currentUser }) => {
   const { username, first_name, profile_image_url } = currentUser;
   return (
-    <div className="my-3 p-3">
-      <div className="my-6 py-4 is-flex is-justify-content-space-around is-align-items-center is-align-content-center">
-        <div className="image edit-profile-image-wrapper mx-3">
-          <img className="is-rounded" src={profile_image_url || `${process.env.PUBLIC_URL}/dev.png`} alt="profile_image" />
-        </div>
-        <p className="px-2 is-size-1 has-text-centered has-text-weight-bold mx-3">
-          Welcome
-          {first_name ? ` ${first_name} !` : ` ${username} !`}
-        </p>
-      </div>
-      <div className="my-3 px-3">
-        <p className="has-text-centered py-3">
-          To add a profile image and other details, please visit the Edit Profile page
-        </p>
-        <div className="is-flex is-justify-content-center is-align-items-center">
-          <Link to="/edit_profile" className="button is-success my-5">Edit Profile</Link>
-        </div>
-        <div className="py-3 is-flex is-justify-content-center is-align-items-center">
-          <p className="has-text-centered">
-            To get into business, navigate to our
-            <Link className="px-2 has-text-primary has-text-weight-bold" to="/developers">Developers </Link>
-            page and get in touch with them
+    <div className="my-3 mx-auto px-2 md:px-8">
+      <div className="mx-auto my-6 p-4 rounded border border-yellow-500">
+        <div className="p-4 flex justify-center space-x-8 items-center content-center">
+          <div className="w-32 rounded-full">
+            <img className="rounded-full" src={profile_image_url || `${process.env.PUBLIC_URL}/dev.png`} alt="profile_image" />
+          </div>
+          <p className="text-3xl text-primary-800 text-center font-bold">
+            Welcome
+            {first_name ? ` ${first_name} !` : ` ${username} !`}
           </p>
+        </div>
+
+        <hr className="border-1 border-gray-200" />
+
+        <div className="my-8 mx-auto w-10/12 text-gray-800">
+          <div className="flex justify-center">
+            <p className="py-3 leading-loose">
+              To add a profile image and other details, please visit the Edit Profile page
+              <Link to="/edit_profile" className="btn btn-warning ml-4">Edit&nbsp;Profile</Link>
+            </p>
+          </div>
+          <div className="flex justify-center">
+            <p className="py-3">
+              To get into business, navigate to our
+              <Link className="px-2 link-primary" to="/developers">Developers </Link>
+              page and get in touch with them
+            </p>
+          </div>
         </div>
       </div>
     </div>
